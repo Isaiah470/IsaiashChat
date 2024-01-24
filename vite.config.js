b89b5b1b6bf8e7f4f19519d0cfd54e737a392cc9
nlpdev2 <157401304+nlpdev2@users.noreply.github.com>
2024-01-24 16:41:24 +0000

    xdfss

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/IsaiashChat/",
   plugins: [svgr(), react()],
   server: {
     host: '0.0.0.0',
