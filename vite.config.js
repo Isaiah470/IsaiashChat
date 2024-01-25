6ad1dfb755b362fe25e806c8258eccf910aa5ec0
Isaiah470 <155671646+Isaiah470@users.noreply.github.com>
2024-01-25 19:17:22 +0000

    o[pp[op[p

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from "vite-plugin-svgr";
-// https://vitejs.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
+
+const options = {
+  workbox: { globPatterns: ["**/*"] },
+  includeAssets: ["**/*"],
+}
+
 export default defineConfig({
   base: "/IsaiashChat/",
-  plugins: [svgr(), react()],
+  plugins: [svgr(), react(), VitePWA(options)],
   server: {
     host: '0.0.0.0',
   }
